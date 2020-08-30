4f5aa117fbd9c3a55739e9d1fc55889e870d0706
Andres <juanxxi2015@gmail.com>
2020-08-30 01:01:47 -0500

    Map Working a loaded
    The import data is not working yet :heart:

--- GMap_Load_DataSet/GUI/Map.Designer.cs
+++ GMap_Load_DataSet/GUI/Map.Designer.cs
@@ -28,13 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mapa = new GMap.NET.WindowsForms.GMapControl();
             this.SuspendLayout();
+            // 
+            // mapa
+            // 
+            this.mapa.Bearing = 0F;
+            this.mapa.CanDragMap = true;
+            this.mapa.EmptyTileColor = System.Drawing.Color.Navy;
+            this.mapa.GrayScaleMode = false;
+            this.mapa.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.mapa.LevelsKeepInMemmory = 5;
+            this.mapa.Location = new System.Drawing.Point(12, 12);
+            this.mapa.MarkersEnabled = true;
+            this.mapa.MaxZoom = 2;
+            this.mapa.MinZoom = 2;
+            this.mapa.MouseWheelZoomEnabled = true;
+            this.mapa.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.mapa.Name = "mapa";
+            this.mapa.NegativeMode = false;
+            this.mapa.PolygonsEnabled = true;
+            this.mapa.RetryLoadTile = 0;
+            this.mapa.RoutesEnabled = true;
+            this.mapa.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.mapa.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.mapa.ShowTileGridLines = false;
+            this.mapa.Size = new System.Drawing.Size(776, 426);
+            this.mapa.TabIndex = 0;
+            this.mapa.Zoom = 0D;
             // 
             // Map
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mapa);
             this.Name = "Map";
             this.Text = "Map";
             this.Load += new System.EventHandler(this.Map_Load);
@@ -43,5 +71,7 @@
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl mapa;
     }
 }
--- GMap_Load_DataSet/GUI/Map.cs
+++ GMap_Load_DataSet/GUI/Map.cs
@@ -27,27 +27,13 @@ namespace GMap_Load_DataSet.GUI
         private void Map_Load(object sender, EventArgs e)
         {
             {
-                gmap.MapProvider = GMapProviders.GoogleMap;
-                gmap.MinZoom = 3;
-                gmap.MaxZoom = 17;
-                gmap.Zoom = 17;
-                gmap.CanDragMap = true;
-                gmap.Manager.Mode = AccessMode.ServerAndCache;
-                overlayOne = new GMapOverlay(gmap, "OverlayOne");
-                lsMarcas.Add("-16.438389, -71.528358");
-                lsMarcas.Add("-16.438606, -71.527878");
-                lsMarcas.Add("-16.438465, -71.527824");
-                lsMarcas.Add("-16.437904, -71.527580");
-                gmap.Position = new PointLatLng(-16.438389, -71.528358);
-                foreach (string cad in lsMarcas)
-                {
-                    Double lat, lon;
-                    string[] aCad = cad.Split(',');
-                    lat = Convert.ToDouble(aCad[0]);
-                    lon = Convert.ToDouble(aCad[1]);
-                    overlayOne.Markers.Add(new GMap.NET.WindowsForms.Markers.GMapMarkerGoogleGreen(new PointLatLng(lat, lon)));
-                    gmap.Overlays.Add(overlayOne);
-                }
+                mapa.DragButton = MouseButtons.Left;
+                mapa.CanDragMap = true;
+                mapa.MapProvider = GMapProviders.OpenStreetMap;
+                mapa.MinZoom = 0;
+                mapa.MaxZoom = 100;
+                mapa.Zoom = 9;
+                mapa.AutoScroll = true;
             }
         }
     }
